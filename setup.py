7d4918c1831a29706db25a263fce6e412c90eed8
Thomas Steinacher <tom@eggdrop.ch>
2012-11-12 20:40:27 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+# Stops exit traceback on tests
+try:
+    import multiprocessing
+except:
+   pass
+
+setup(
+    name='cleancat',
+    version='0.1',
+    url='http://github.com/elasticsales/cleancat',
+    license='BSD',
+    author='Thomas Steinacher',
+    author_email='cleancat@thomasst.ch',
+    maintainer='Thomas Steinacher',
+    maintainer_email='cleancat@thomasst.ch',
+    description='Validation library for Python designed to be used with JSON REST frameworks',
+    long_description=__doc__,
+    packages=[
+        'cleancat',
+    ],
+    test_suite='nose.collector',
+    zip_safe=False,
+    platforms='any',
+    setup_requires=[
+        'nose',
+        'coverage',
+        'python-dateutil',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
