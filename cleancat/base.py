d9256fab648a47d2a94959a936ff858f4f574c03
Thomas Steinacher <tom@eggdrop.ch>
2017-10-31 10:58:29 -0400

    Fix MongoReference serialization for None values (#23)

--- cleancat/base.py
+++ cleancat/base.py
@@ -465,7 +465,8 @@ class MongoReference(Field):
             raise ValidationError('Object does not exist.')
 
     def serialize(self, value):
-        return value.pk
+        if value:
+            return value.pk
 
 class Schema(object):
     """
