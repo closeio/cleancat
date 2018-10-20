bc6ced1c58c6c90bfdb83ee430175cb2e05f4d99
Stefan Wojcik <wojcikstefan@gmail.com>
2018-10-20 18:02:07 -0400

    Pytest-ify sqlalchemy tests

--- tests/test_sqla.py
+++ tests/test_sqla.py
@@ -1,24 +1,42 @@
-import unittest
-
 import pytest
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from cleancat import Integer, Schema, String, ValidationError
+from cleancat import Integer, Schema, StopValidation, String, ValidationError
 from cleancat.sqla import SQLAEmbeddedReference, SQLAReference, object_as_dict
 
 
-def test_object_as_dict():
-    Base = declarative_base()
+@pytest.fixture
+def decl_base():
+    return declarative_base()
 
-    class Person(Base):
+
+@pytest.fixture
+def person_cls(decl_base):
+    class Person(decl_base):
         __tablename__ = 'cleancattest'
         id = sa.Column(sa.Integer, primary_key=True)
         name = sa.Column(sa.String)
         age = sa.Column(sa.Integer)
+    return Person
 
-    steve = Person(name='Steve', age=30)
+
+@pytest.fixture
+def sqla_session(decl_base, person_cls):
+    """Set up an SQLA connection, create all tables, and return a session."""
+    engine = sa.create_engine('sqlite:///:memory:')
+    decl_base.metadata.create_all(engine)
+    session = scoped_session(sessionmaker(bind=engine))
+
+    # Set up a ModelClass.query "shortcut" property for all models.
+    person_cls.query = session.query_property()
+
+    return session
+
+
+def test_object_as_dict(person_cls):
+    steve = person_cls(name='Steve', age=30)
     assert object_as_dict(steve) == {
         'id': None,
         'age': 30,
@@ -26,38 +44,47 @@ def test_object_as_dict():
     }
 
 
-class SQLABaseTestCase(unittest.TestCase):
+@pytest.mark.usefixtures('sqla_session')
+class TestSQLAReferenceField:
 
-    def setUp(self):
-        super(SQLABaseTestCase, self).setUp()
+    def test_it_updates_an_existing_instance(self, person_cls, sqla_session):
+        steve = person_cls(name='Steve', age=30)
+        sqla_session.add(steve)
+        sqla_session.commit()
 
-        Base = declarative_base()
+        clean_val = SQLAReference(person_cls).clean(str(steve.id))
+        assert isinstance(clean_val, person_cls)
+        assert clean_val.id == steve.id
 
-        class Person(Base):
-            __tablename__ = 'cleancattest'
-            id = sa.Column(sa.Integer, primary_key=True)
-            name = sa.Column(sa.String)
-            age = sa.Column(sa.Integer)
+    def test_updating_missing_instance_fails(self, person_cls):
+        with pytest.raises(ValidationError) as e:
+            SQLAReference(person_cls).clean('id-that-does-not-exist')
+        assert unicode(e.value) == 'Object does not exist.'
 
-        self.Person = Person
-
-        engine = sa.create_engine('sqlite:///:memory:')
-        Base.metadata.create_all(engine)
-        self.session = scoped_session(sessionmaker(bind=engine))
-        self.Person.query = self.session.query_property()
+    def test_it_can_be_optional(self, person_cls):
+        field = SQLAReference(person_cls, required=False)
+        with pytest.raises(StopValidation) as e:
+            field.clean(None)
+        assert e.value.args[0] is None
 
 
-class SQLAEmbeddedReferenceTestCase(SQLABaseTestCase):
+@pytest.mark.usefixtures('sqla_session')
+class TestSchemaWithSQLAEmbeddedReference:
 
-    def test_create(self):
+    @pytest.fixture
+    def schema_cls(self, person_cls):
         class PersonSchema(Schema):
             name = String()
             age = Integer()
 
         class BookSchema(Schema):
-            author = SQLAEmbeddedReference(self.Person, PersonSchema)
+            author = SQLAEmbeddedReference(person_cls, PersonSchema)
+            title = String(required=False)
 
-        schema = BookSchema({
+        return BookSchema
+
+    def test_it_creates_a_new_instance(self, schema_cls, person_cls):
+        schema = schema_cls({
             'author': {
                 'name': 'New Author',
                 'age': 30
@@ -65,24 +92,18 @@ class SQLAEmbeddedReferenceTestCase(SQLABaseTestCase):
         })
         data = schema.full_clean()
         author = data['author']
-        assert isinstance(author, self.Person)
+        assert isinstance(author, person_cls)
         assert not author.id
         assert author.name == 'New Author'
         assert author.age == 30
 
-    def test_update(self):
-        class PersonSchema(Schema):
-            name = String()
-            age = Integer()
+    def test_it_updates_an_existing_instance(self, schema_cls, person_cls,
+                                             sqla_session):
+        steve = person_cls(name='Steve', age=30)
+        sqla_session.add(steve)
+        sqla_session.commit()
 
-        class BookSchema(Schema):
-            author = SQLAEmbeddedReference(self.Person, PersonSchema)
-
-        steve = self.Person(name='Steve', age=30)
-        self.session.add(steve)
-        self.session.commit()
-
-        schema = BookSchema({
+        schema = schema_cls({
             'author': {
                 'id': str(steve.id),
                 'name': 'Updated',
@@ -91,20 +112,13 @@ class SQLAEmbeddedReferenceTestCase(SQLABaseTestCase):
         })
         data = schema.full_clean()
         author = data['author']
-        assert isinstance(author, self.Person)
+        assert isinstance(author, person_cls)
         assert author.id == steve.id
         assert author.name == 'Updated'
         assert author.age == 50
 
-    def test_update_missing(self):
-        class PersonSchema(Schema):
-            name = String()
-            age = Integer()
-
-        class BookSchema(Schema):
-            author = SQLAEmbeddedReference(self.Person, PersonSchema)
-
-        schema = BookSchema({
+    def test_updating_missing_instance_fails(self, schema_cls):
+        schema = schema_cls({
             'author': {
                 'id': 123456789,
                 'name': 'Arbitrary Non-existent ID'
@@ -113,64 +127,9 @@ class SQLAEmbeddedReferenceTestCase(SQLABaseTestCase):
         pytest.raises(ValidationError, schema.full_clean)
         assert schema.field_errors == {'author': 'Object does not exist.'}
 
-    def test_optional(self):
-        class PersonSchema(Schema):
-            name = String()
-
-        class BookSchema(Schema):
-            title = String()
-            author = SQLAEmbeddedReference(self.Person, PersonSchema,
-                                           required=False)
-
-        schema = BookSchema({
-            'title': 'Book without an author',
-            'author': None
-        })
-        data = schema.full_clean()
-        assert data == {
-            'title': 'Book without an author',
-            'author': None
-        }
-
-
-class SQLAReferenceTestCase(SQLABaseTestCase):
-
-    def test_existing(self):
-        class BookSchema(Schema):
-            title = String()
-            author = SQLAReference(self.Person)
-
-        steve = self.Person(name='Steve', age=30)
-        self.session.add(steve)
-        self.session.commit()
-
-        schema = BookSchema({
-            'title': "Steve's Book",
-            'author': str(steve.id),
-        })
-        data = schema.full_clean()
-        author = data['author']
-        assert isinstance(author, self.Person)
-        assert author.id == steve.id
-
-    def test_missing(self):
-        class BookSchema(Schema):
-            title = String()
-            author = SQLAReference(self.Person)
-
-        schema = BookSchema({
-            'title': 'Book without an invalid author',
-            'author': 'id-that-does-not-exist'
-        })
-        pytest.raises(ValidationError, schema.full_clean)
-        assert schema.field_errors == {'author': 'Object does not exist.'}
-
-    def test_optional(self):
-        class BookSchema(Schema):
-            title = String()
-            author = SQLAReference(self.Person, required=False)
-
-        schema = BookSchema({
+    def test_it_can_be_optional(self, schema_cls):
+        schema_cls.author.required = False
+        schema = schema_cls({
             'title': 'Book without an author',
             'author': None
         })
