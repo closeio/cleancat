dfeb32844b4cad196b3612156a27f45df5797905
Stefan Wojcik <wojcikstefan@gmail.com>
2018-10-20 17:31:07 -0400

    Pytest-ify mongo tests

--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -2,49 +2,45 @@ import pytest
 from bson import ObjectId
 from mongoengine import Document, StringField, connect
 
-from cleancat import Schema, String, ValidationError
+from cleancat import Schema, StopValidation, String, ValidationError
 from cleancat.mongo import MongoEmbeddedReference, MongoReference
-from cleancat.utils import ValidationTestCase
 
 
-class MongoValidationTestCase(ValidationTestCase):
-
-    def setUp(self):
-        super(MongoValidationTestCase, self).setUp()
-
-        connect(db='cleancat_test')
-
-        class Person(Document):
-            name = StringField()
-
-        Person.drop_collection()
-        self.Person = Person
+@pytest.fixture
+def mongodb():
+    connect(db='cleancat_test')
 
 
-class MongoReferenceTestCase(MongoValidationTestCase):
+@pytest.fixture
+def person_cls(mongodb):
+    class Person(Document):
+        name = StringField()
+    Person.drop_collection()
+    return Person
 
-    def test_existing(self):
+
+class TestMongoReferenceField:
+
+    def test_it_accepts_an_existing_doc(self, person_cls):
+        field = MongoReference(person_cls)
+        doc = person_cls.objects.create(name='Steve')
+        assert field.clean(str(doc.pk)) == doc
+
+    def test_it_rejects_a_missing_doc(self, person_cls):
+        field = MongoReference(person_cls)
+        with pytest.raises(ValidationError) as e:
+            field.clean(str(ObjectId()))
+        assert unicode(e.value) == 'Object does not exist.'
+
+    def test_it_can_be_optional(self, person_cls):
+        field = MongoReference(person_cls, required=False)
+        with pytest.raises(StopValidation) as e:
+            field.clean(None)
+        assert e.value.args[0] is None
+
+    def test_rejection_with_raw_field_name_in_schema(self, person_cls):
         class BookSchema(Schema):
-            author_id = MongoReference(self.Person)
-
-        doc = self.Person.objects.create(name='Steve')
-        self.assertValid(
-            BookSchema({'author_id': str(doc.pk)}),
-            {'author_id': doc}
-        )
-
-    def test_missing(self):
-        class BookSchema(Schema):
-            author_id = MongoReference(self.Person)
-
-        self.assertInvalid(
-            BookSchema({'author_id': str(ObjectId())}),
-            {'field-errors': ['author_id']}
-        )
-
-    def test_missing_with_raw_field_name(self):
-        class BookSchema(Schema):
-            author = MongoReference(self.Person, raw_field_name='author_id')
+            author = MongoReference(person_cls, raw_field_name='author_id')
 
         schema = BookSchema({'author_id': str(ObjectId())})
         pytest.raises(ValidationError, schema.full_clean)
@@ -52,52 +48,35 @@ class MongoReferenceTestCase(MongoValidationTestCase):
             'author_id': 'Object does not exist.'
         }
 
-    def test_optional(self):
-        class BookSchema(Schema):
-            title = String()
-            author_id = MongoReference(self.Person, required=False)
 
-        schema = BookSchema({
-            'title': 'Book without an author',
-            'author_id': None
-        })
-        data = schema.full_clean()
-        assert data == {
-            'title': 'Book without an author',
-            'author_id': None
-        }
+class TestSchemaWithMongoEmbeddedReferenceField:
 
-
-class MongoEmbeddedReferenceTestCase(MongoValidationTestCase):
-
-    def test_create(self):
+    @pytest.fixture
+    def schema_cls(self, person_cls):
         class PersonSchema(Schema):
             name = String()
 
         class BookSchema(Schema):
-            author = MongoEmbeddedReference(self.Person, PersonSchema)
+            author = MongoEmbeddedReference(person_cls, PersonSchema)
+            title = String(required=False)
 
-        schema = BookSchema({
+        return BookSchema
+
+    def test_it_creates_a_new_instance(self, schema_cls, person_cls):
+        schema = schema_cls({
             'author': {
                 'name': 'New Author'
             }
         })
         data = schema.full_clean()
         author = data['author']
-        assert isinstance(author, self.Person)
+        assert isinstance(author, person_cls)
         assert not author.pk
         assert author.name == 'New Author'
 
-    def test_update(self):
-        class PersonSchema(Schema):
-            name = String()
-
-        class BookSchema(Schema):
-            author = MongoEmbeddedReference(self.Person, PersonSchema)
-
-        doc = self.Person.objects.create(name='Steve')
-
-        schema = BookSchema({
+    def test_it_updates_an_existing_instance(self, schema_cls, person_cls):
+        doc = person_cls.objects.create(name='Steve')
+        schema = schema_cls({
             'author': {
                 'id': str(doc.pk),
                 'name': 'Updated'
@@ -105,18 +84,12 @@ class MongoEmbeddedReferenceTestCase(MongoValidationTestCase):
         })
         data = schema.full_clean()
         author = data['author']
-        assert isinstance(author, self.Person)
+        assert isinstance(author, person_cls)
         assert author.pk == doc.pk
         assert author.name == 'Updated'
 
-    def test_update_missing(self):
-        class PersonSchema(Schema):
-            name = String()
-
-        class BookSchema(Schema):
-            author = MongoEmbeddedReference(self.Person, PersonSchema)
-
-        schema = BookSchema({
+    def test_updating_missing_instance_fails(self, schema_cls):
+        schema = schema_cls({
             'author': {
                 'id': str(ObjectId()),
                 'name': 'Arbitrary Non-existent Object ID'
@@ -125,16 +98,9 @@ class MongoEmbeddedReferenceTestCase(MongoValidationTestCase):
         pytest.raises(ValidationError, schema.full_clean)
         assert schema.field_errors == {'author': 'Object does not exist.'}
 
-    def test_optional(self):
-        class PersonSchema(Schema):
-            name = String()
-
-        class BookSchema(Schema):
-            title = String()
-            author = MongoEmbeddedReference(self.Person, PersonSchema,
-                                            required=False)
-
-        schema = BookSchema({
+    def test_it_can_be_optional(self, schema_cls):
+        schema_cls.author.required = False
+        schema = schema_cls({
             'title': 'Book without an author',
             'author': None
         })
