59df1422b3ebea9477026ca80cd7af1ae9734d89
Stefan Wojcik <wojcikstefan@gmail.com>
2018-10-29 14:33:46 -0400

    v0.7.7

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.version_info[:2] < (3, 4):
 
 setup(
     name='cleancat',
-    version='0.7.6',
+    version='0.7.7',
     url='http://github.com/elasticsales/cleancat',
     license='MIT',
     author='Thomas Steinacher',
